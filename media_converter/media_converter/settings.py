d07674bce2605565a379a942f99e702260b07c30
Obinna Akpulonu <akpulonujoseph@gmail.com>
2025-02-12 21:14:18 +0100

    Update settings.py

--- media_converter/media_converter/settings.py
+++ media_converter/media_converter/settings.py
@@ -16,9 +16,6 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'your_secret_key'
 
